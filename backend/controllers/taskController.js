e68495e381f20a073bb9829a34a07e2a6ca541b7
Neto-M <neto83454@gmail.com>
2026-09-02 15:25:21 -0300

    finalização dos controllers

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,19 +1,57 @@
-
+const Task = require('../models/Task')
+const getUserByToken = require('../helpers/get-user-by-token')
 
 module.exports = class taskController {
     static async createTask (req, res) {
-        
+        const id = req.params.id
+
+        const newTask = {
+            taskname: req.body.taskname,
+            TitleId: id,
+        }
+
+        if(!newTask || '') {
+            res.status(211).json({message: 'Sua lista de tarefas está vazia. Escreva uma tarefa!'})
+            return
+        }
+
+        try {
+            await Task.create(newTask)
+        } catch (error) {
+            res.status(500).json({error: error.message})
+        }
     }
 
     static async getTask (req, res) {
+        const id = req.params.id
 
+        try {
+            await Task.findAll({where: {TitleId: id}})
+        } catch (error) {
+            res.status(500).json({error: error.message})
+        }
     }
 
     static async editTask (req, res) {
+        const id = req.params.id
+        const updateTask = {
+            taskname: req.body.taskname
+        }
 
+        try {
+            await Task.update(updateTask, {where: {id: id}})
+        } catch (error) {
+            res.status(500).json({error: error.message})
+        }
     }
 
     static async removeTask (req, res) {
+        const id = req.params.id
 
+        try {
+            await Task.destroy({where: {id: id}})
+        } catch (error) {
+            res.status(500).json({error: error.message})
+        }
     }
 }
--- backend/controllers/titleController.js
+++ backend/controllers/titleController.js
@@ -25,14 +25,44 @@ module.exports = class titleController {
     }
 
     static async getTitle (req, res) {
+        const token = req.cookies.token
+        const user = getUserByToken(token)
 
+        try {
+            const titles = await Title.findAll({where: {UserId: user.id}})
+            if(titles.length === 0) {
+                res.status(200).json({message: 'Sem Tasks'})
+            }
+        } catch (error) {
+            res.status(500).json({error: error.message})
+        }
     }
 
     static async editTitle (req, res) {
+        const id = req.params.id
+        const updateTitle = {
+            title: req.body.title
+        }
 
+        if(!updateTitle || '') {
+            updateTitle = 'Sem Título'
+        }
+
+        try {
+            await Title.update(updateTitle, {where: {id: id}})
+        } catch (error) {
+            res.status(500).json({error: error.message})
+        }
     }
 
     static async removeTitle (req, res) {
+        const id = req.params.id
 
+        try {
+            await Title.destroy({where: {id: id}})
+            res.status(200).json({message: 'Task deletada com sucesso!'})
+        } catch (error) {
+            res.status(500).json({error: error.message})
+        }
     }
 }
